b82c389d95e80ca1f7b36eeff278d34ea03ebfbc
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-08-10 00:41:23 -0500

    Adding Framework and basic landing page

---
+++ app/views/index.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Libro Libre :: México</title>
+    <link rel="stylesheet" href="foundation/css/foundation.css" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/general_style.css" />
+    <script src="foundation/js/vendor/modernizr.js"></script>
+</head>
+<body>
+
+<!-- <nav class="top-bar" data-topbar>
+
+    <section class="top-bar-section">
+        <ul class="right">
+            <li><a href="#"> Quienes somos.</a></li>
+            <li class="has-dropdown">
+                <a href="#">About</a>
+                <ul class="dropdown">
+                    <li><a href="#">Contribute</a></li>
+                    <li><a href="#">About Us</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+</nav> -->
+
+<div class="row text-center">
+    <div class="large-12 columns">
+        <h1>Libro Libre :: México</h1>
+    </div>
+</div>
+
+
+<article class="text-left">
+    <div class="row" style="text-align: center; border-top: solid 1px black; padding-top: 1em;">
+        <div class="small-4 medium-4 large-4 columns">
+            Has feliz a otra persona con los libros que ya no ocupas.
+        </div>
+        <div class="small-4 medium-4 large-4 columns">
+                Obten libros que necesites.
+        </div>
+        <div class="small-4 medium-4 large-4 columns">
+                Contribuye a mejorar la cultura de tu comunidad.
+        </div>
+    </div>
+    <div class = "row">
+     <div class="small-12columns">
+            <img src="img/librero.jpg" alt="Librero" style="margin-top: 2em"/>
+        </div>
+    </div>
+
+</article>
+
+
+<script src="foundation/js/vendor/jquery.js"></script>
+<script src="foundation/js/foundation.min.js"></script>
+<script>
+    $(document).foundation();
+    $(document).ready(function(){
+        h = $(window).height();
+        $("body").height(h);
+    });
+</script>
+</body>
+</html>
